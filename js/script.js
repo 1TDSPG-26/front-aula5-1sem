9e1ebef18bf83ae04f4379a72dc789ac690540b6
carlostsucamoto <caduchiarelli@hotmail.com>
2026-05-11 09:24:39 -0300

    fix: refatorando o projeto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
+
+//Capturando o botão de entrar
 const botaoEntrar = document.getElementById("btnEntrar");
-botaoEntrar.addEventListener("click", function(){
-    console.log("Botão de entrar clicado");
-});
+console.log(botaoEntrar);
+
+
+//Atrelando ao botão uma função que vai receber dois parâmetros.
+// O primeiro será o evento de tela, o segundo será a função de callBack ou função anônima.
+// botaoEntrar.addEventListener("click", function(){
+//     console.log("Botão de entrar clicado");
+
+
+//     //Para casa:
+//     //Capturar os campos de email e senha.
+//     //Armazenar em variáveis.
+//     //Imprimir seus valores.
+
+ 
+
+
+// });
