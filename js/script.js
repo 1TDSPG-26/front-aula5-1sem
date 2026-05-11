715a016ae78ab2aa87703d93322de34bb1da453b
manuelalramos <manuela.lima.r@outlook.com>
2026-05-11 09:53:58 -0300

    feat : Usando JS para imprimir e capturar elementos.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 // Capturando o botão de entrar 
 const botaoEntrar = document.getElementById("btnEntrar");
-
-console.log(botaoEntrar)
 //Função de dois parâmetros no botão. Evento de tela e função de callBack ou função anônima
-botaoEntrar.addEventListener("evento", function(){
-    console.log("Botão de entrar clicado");
+botaoEntrar.addEventListener("mouseover", function (){
 
-    
+    const inputEmail = document.getElementById("idEmail");
+    const inputSenha = document.getElementById("idSenha");
+
+//Dados em objeto
+//Declarando
+const usuario = {
+    email : inputEmail.value,
+    senha : inputSenha.value
+};
+
+console.log(`usuário ${usuario}`)
+
+    console.log(`Email ${inputEmail.value} e senha ${inputSenha.value}`);
 });
+
+
+
