89a1d09bfacefea2b88f5d8f0cbd8029694e3857
giovannidevazevedo <giovanniazevedo07@gmail.com>
2026-05-11 09:03:46 -0300

    feat : fazendo outros pages e sitema de login

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-// alert("olá mundo!");
-// var nome = prompt("Olá digite seu nome");
 
-// alert("Meu nome é " + nome);
-// console.log("Meu nome é " + nome);
-let nome = "giovanni"
-let status = false
-let idade = 19
+//Capturando o botão de entrar
+const botaoEntrar = document.getElementById("btnEntrar");
 
-console.log(`${nome} de ${idade} anos esta ${(status === true ? "empregado": "desenpregado")} atualmente`)
+console.log(botaoEntrar);
+
+
+//Atrelando ao botão uma função que vai receber dois parâmetros.
+// O primeiro será o evento de tela, o segundo será a função de callBack ou função anônima.
+botaoEntrar.addEventListener("click", function(){
+    console.log("Botão de entrar clicado");
+
+
+    //Para casa:
+    //Capturar os campos de email e senha.
+    //Armazenar em variáveis.
+    //Imprimir seus valores.
+
+
+
+
+});
