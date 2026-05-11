c0ddbb10288f34f22eeedc64bb023a684fb5bd51
Pedro-H-Salvatore <pedrosalvatore2112@gmail.com>
2026-05-11 09:54:56 -0300

    feat: Manipulação do DOM com JavaScript na coleta dos dados de login do usuário.

--- js/script.js
+++ js/script.js
@@ -4,5 +4,20 @@ const botaoEntrar = document.getElementById("btnEntrar");
 //atrelando o botão uma função que vai receber dois parâmetros.
 //o primeiro será o evento de tela, o segundo será a função de callback ou função anonima.
 botaoEntrar.addEventListener("click", function(){
-    console.log("Botão está ativo.");
+   
+ console.log("Botão está ativo.");
+    
+
+ const botaoEmail = document.getElementById("idEmail");  
+ const botaoSenha = document.getElementById("idSenha");
+
+ const usuario = {
+    botaoEmail : botaoEmail.value,
+    botaoSenha : botaoSenha.value
+
+ }
+    console.log(`email : ${botaoEmail.value} senha: ${botaoSenha.value}`);
 })
+
+
+
