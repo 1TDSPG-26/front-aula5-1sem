ee340151aa6d4d82b65aa1487933aa681d7e31df
yasminmartins18 <yasmin.s.s.martins1804@gmail.com>
2026-05-11 09:52:58 -0300

    feat: Manipulação do DOM, recuperando dados de entrada do usuário

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
-// capturando o botão de entrar
+//capturando o botão de entrar
 const botaoEntrar = document.getElementById("btnEntrar");
 
 //atrelando ao botão uma função que vai receber dos parâmetros
 // o primeiro será o evento de tela, o segundo será a função anônima ou a funçaõ de callBack
-botaoEntrar.addEventListener("evento", function(){
-    console.log("botão de entrar clicando");
+botaoEntrar.addEventListener("click", function(){
+
+  //Imprimir seus valores.
+
+    const email = document.getElementById("email");
+    const senha = document.getElementById("senha");
+
+    // Armazenando os dados em um Objeto:
+    //declarando um objeto
+    const usuario = {
+        email : email.value,
+        senha : senha.value
+    }
     
+    //Imprimindo os dados dos campos:
+
+    //Concatenação padrão:
+    console.log("Email : " + email.value + " e senha: " + senha.value);
+    
+    //Impressão dos dados com Template Literal e interpolação:
+    console.log(`Email :  ${email.value}  e senha: ${senha.value}.`);
+    
+    //Apresente os dados armazenados no Objeto usuário utilizando Template Literal;
+    //Realizar todos os exercícos da apostila até a pág 64;
 });
