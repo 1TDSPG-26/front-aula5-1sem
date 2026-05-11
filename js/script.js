21b38217d446f718872f8561083b118c14f430b3
DiegoGomes <diegoggomeslima@gmail.com>
2026-05-11 09:52:45 -0300

    feat : ajuste login e armazenamento de objetos

--- js/script.js
+++ js/script.js
@@ -1,30 +1,32 @@
-<<<<<<< HEAD
-//alert("olá mundo!");
-//var nome = prompt("Olá digite seu nome");
-
-//alert("Meu nome é " + nome);
-//console.log("Meu nome é " + nome);
-=======
 
 //Capturando o botão de entrar
 const botaoEntrar = document.getElementById("btnEntrar");
-
 console.log(botaoEntrar);
 
 
 //Atrelando ao botão uma função que vai receber dois parâmetros.
 // O primeiro será o evento de tela, o segundo será a função de callBack ou função anônima.
+
+
 botaoEntrar.addEventListener("click", function(){
-    console.log("Botão de entrar clicado");
+    //     //Para casa:
+//     //Capturar os campos de email e senha.
+//     //Armazenar em variáveis.
+//     //Imprimir seus valores.
 
-
-    //Para casa:
-    //Capturar os campos de email e senha.
-    //Armazenar em variáveis.
-    //Imprimir seus valores.
-
-
-
-
+    const email = document.getElementById("idEmail");
+    const senha = document.getElementById("idSenha");
+    
+    //Armazenando os dados em um Objeto:
+    //Declarando um Objeto 
+    const usuario = {
+        email : email.value
+        senha : senha.value
+    }
+    
+    //Imprimindo os dados dos campos:
+    //Impressão dos dados com Template Literal e interpolação:
+    console.log(`Email :  ${email.value}  e senha: ${senha.value}.`);
+    
+    //Apresente os dados armazenados no Objeto usuário utilizando Template Literal:
 });
->>>>>>> main
